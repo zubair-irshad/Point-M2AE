0f8ed6dadfc45783aa2916be30c402a5fefe9589
Zubair Irshad <Muhammad Zubair Irshad>
2024-03-04 11:51:15 -0800

    add nerfmae segmentation dataset

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,4 +1,5 @@
-from .runner_pretrain import run_net as pretrain_run_net
+# from .runner_pretrain import run_net as pretrain_run_net
+from .runner_pretrain_nerfmae import run_net as pretrain_run_net
 from .runner_test_svm import run_net_svm_modelnet40 as test_svm_run_net_modelnet40
 from .runner_test_svm import run_net_svm_scan as test_svm_run_net_scan
 from .runner_finetune import run_net as finetune_run_net
